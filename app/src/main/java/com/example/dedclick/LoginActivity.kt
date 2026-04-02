d29ec32cf58a6b0622f850ee1d0f3adb33129602
ArtemBrychev <chosen_one05@mail.ru>
2026-04-02 23:02:21 +0300

    Began working on ui interactions

---
+++ app/src/main/java/com/example/dedclick/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.example.dedclick
+
+class LoginActivity {
+}
--- app/src/main/java/com/example/dedclick/MainActivity.kt
+++ app/src/main/java/com/example/dedclick/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.dedclick
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.example.dedclick.databinding.ActivityStartBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityStartBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_start)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val loginButton = binding.loginButton
+        loginButton.setOnClickListener {
+            binding.loginButtonText.text = "нажато"
+        }
+
+        val registerButton = binding.registerButton
+        registerButton.setOnClickListener {
+            binding.registerButtonText.text = "нажато"
+        }
     }
 }
